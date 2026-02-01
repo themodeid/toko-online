8cade2f56149eeba715571266c49746be750a74c
themodeid <adamwahyukur@gmail.com>
2026-02-01 14:36:17 +0700

    login nanti dulu saya hapus dulu

--- backend/src/modules/produk/produk.route.ts
+++ backend/src/modules/produk/produk.route.ts
@@ -1,18 +1,19 @@
 import { Router } from "express";
-import * as controller from "./produk.controller"
-import { validateBody } from "../../middlewares/validateBody"
-import { produkSchema, updateProdukSchema } from "./produk.schema"
-import { protect, restrictTo } from "../../middlewares/auth.middleware"
+import * as controller from "./produk.controller";
+import { validateBody } from "../../middlewares/validateBody";
+import { produkSchema, updateProdukSchema } from "./produk.schema";
 
-const router = Router()
+const router = Router();
 
-// Semua user bisa melihat produk
-router.get("/", controller.getAllProduk)
-router.get("/:id", controller.getProdukById)
+// Public
+router.get("/", controller.getAllProduk);
+router.get("/:id", controller.getProdukById);
 
-// Hanya owner yang bisa create, update, delete produk
-router.post("/", protect, restrictTo("owner"), validateBody(produkSchema), controller.createProduk)
-router.put("/:id", protect, restrictTo("owner"), validateBody(updateProdukSchema), controller.updateProduk)
-router.delete("/:id", protect, restrictTo("owner"), controller.deleteProduk)
+// Semua user login bisa CRUD
+router.post("/", validateBody(produkSchema), controller.createProduk);
+
+router.patch("/:id", validateBody(updateProdukSchema), controller.updateProduk);
+
+router.delete("/:id", controller.deleteProduk);
 
 export default router;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import kontakRoutes from "../modules/kontak/kontak.route";
-import loginRoutes from "../modules/login_register/login.route";
 import produkRoutes from "../modules/produk/produk.route";
 
 const router = Router();
 
 router.use("/kontak", kontakRoutes);
 router.use("/produk", produkRoutes);
-router.use("/", loginRoutes);
 
 export default router;
