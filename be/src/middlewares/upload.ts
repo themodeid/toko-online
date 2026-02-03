dda0b188e04c21d3d5a7c7dd705bd859459ecffc
themodeid <adamwahyukur@gmail.com>
2026-02-03 16:03:52 +0700

    menambahkan fitru upload foto di backend

---
+++ be/src/middlewares/upload.ts
@@ -0,0 +1,26 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../../uploads/"));
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueName + path.extname(file.originalname));
+  },
+});
+
+export const upload = multer({
+  storage,
+  limits: {
+    fileSize: 2 * 1024 * 1024, // 2MB
+  },
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.startsWith("image/")) {
+      cb(new Error("File harus berupa gambar"));
+    } else {
+      cb(null, true);
+    }
+  },
+});
--- be/src/routes/index.ts
+++ be/src/routes/index.ts
@@ -2,11 +2,28 @@ import { Router } from "express";
 import kontakRoutes from "../modules/kontak/kontak.route";
 import produkRoutes from "../modules/produk/produk.route";
 import registerRoutes from "../modules/login-register/register.route";
+import { upload } from "../middlewares/upload";
 
 const router = Router();
 
 router.use("/kontak", kontakRoutes);
 router.use("/produk", produkRoutes);
 router.use("/auth", registerRoutes);
+router.post(
+  "/upload-avatar",
+  upload.single("photo"), // "photo" HARUS sama dengan name di frontend
+  (req, res) => {
+    if (!req.file) {
+      return res.status(400).json({
+        message: "File tidak ditemukan",
+      });
+    }
+
+    res.json({
+      message: "Upload berhasil",
+      filePath: `/uploads/${req.file.filename}`,
+    });
+  },
+);
 
 export default router;
